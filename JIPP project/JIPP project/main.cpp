be52593c1f0edf95f5317cb41a94ac9059527925
PiotrKorczyk <p.k.6@wp.pl>
2017-01-23 15:54:33 +0100

    basic materials class

--- JIPP project/JIPP project/main.cpp
+++ JIPP project/JIPP project/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "wooden_door.h"
+#include "heavy_door.h"
+#include "secure_door.h"
 #include <string>
 
 using namespace std;
@@ -7,15 +9,16 @@ using namespace std;
 
 int main() {
 
-WoodenDoor d1("Normal wood", 50, 1);
+Door *d1 = new WoodenDoor("Normal wood", 50, 1);
+Door *d2 = new HeavyDoor("H door", 200, 0);
+Door *d3 = new SecureDoor("S door", 1000, 1);
 
-d1.doorType();
-d1.setName("Normal wood v2");
+d1->doorType();
+d2->doorType();
+d3->doorType();
 
-d1.setPrice(100);
-
-d1.doorType();
-d1.~WoodenDoor();
+d1->~Door();
+d1->doorType();
 
 system("pause");
 }
--- JIPP project/JIPP project/materials.h
+++ JIPP project/JIPP project/materials.h
@@ -1,1 +1,22 @@
-#pragma once
+#ifndef MATERIALS_H
+#define MATERIALS_H
+
+class Materials {
+private:
+	int amountOfWood = 1000;
+	int amountOfIron = 1000;
+
+public:
+	Materials();
+	~Materials();
+	void materialsInfo();
+
+	int getAmountOfWood();
+	void setAmountOfWood(int _value);
+	int getAmountOfIron();
+	void setAmountOfIron(int _value);
+
+};
+
+
+#endif
